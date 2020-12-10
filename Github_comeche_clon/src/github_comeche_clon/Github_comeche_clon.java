8d5218899c95c9c19ef58a797a72691c75ec219f
adria <adria@LAPTOP-IRC9LRNC.home>
2020-12-10 18:52:08 +0100

    comeche commit hecho 10/12/2020 16:10

--- Github_comeche_clon/src/github_comeche_clon/Github_comeche_clon.java
+++ Github_comeche_clon/src/github_comeche_clon/Github_comeche_clon.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package github_comeche_clon;
+
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -16,7 +12,16 @@ public class Github_comeche_clon {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String texto=JOptionPane.showInputDialog("Introduce un codigo de la tabla ASCII");
+        //Pasamos el String a int
+        int codigo=Integer.parseInt(texto);
+ 
+        //Pasamos el codigo a caracter
+        char caracter=(char)codigo;
+ 
+        System.out.println(caracter);
+    }
     }
     
-}
+
+
